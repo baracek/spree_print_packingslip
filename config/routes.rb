0ad2985bc23c9eb1818c6125031d66f484fd54c0
Bara Cek <baracek@yahoo.com>
2011-07-11 09:41:51 -0400

    Next attempt at rails 3 routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do 
-    resources :shipments, :member => { :packingslip => :get }
+    resources :shipments do
+      member do
+        get :packingslip
+      end
+    end
   end
 end
