438cddda94a910221c7f420ebd1ffec784e5952b
Bara Cek <baracek@yahoo.com>
2011-07-11 09:58:15 -0400

    Next attempt at rails 3 routing

--- lib/spree_print_packingslip_hooks.rb
+++ lib/spree_print_packingslip_hooks.rb
@@ -1,5 +1,5 @@
 class SpreePrintPackingslipHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_shipment_edit_buttons do
-    %( <%= button_link_to("Packing Slip", {:controller=>"admin/shipment", :action=>"packingslip", :format=>:pdf}) %> )
+    %( <%= button_link_to("Packing Slip", packingslip_admin_order_shipment_url( @order, @shipment, :format => :pdf )) %> )
   end
 end
